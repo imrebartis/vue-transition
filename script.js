ad615c003be3d65892fd4ea4fdce6d509f3b605d
Imre Bartis <imre.bartis@siili.com>
2018-01-24 13:11:54 +0200

    simple modal

---
+++ script.js
@@ -0,0 +1,20 @@
+const Child = {
+    template: '#childarea'
+  };
+  
+  new Vue({
+    el: '#app',
+    data() {
+      return {
+        isShowing: false
+      }
+    },
+    methods: {
+      toggleShow() {
+        this.isShowing = !this.isShowing;
+      }
+    },
+    components: {
+      appChild: Child
+    }
+  });
